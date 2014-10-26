e45ffca5333e4de01fa1c161c09fc51d5ce19690
sanjeev sharma <sanjeevcs44@gmail.com>
2014-10-26 10:17:38 +0530

    operator.js

---
+++ operator.js
@@ -0,0 +1,17 @@
+var a = 1;
+var c= 7;
+var b= "c";
+
+//arithematic operation
+console.log(a*a);
+console.log(a+a);
+console.log(a-a);
+console.log(a/a);
+
+assignment operation
+console.log(a+=5);
+
+console.log(a>=c);
+console.log(a>=c);
+
+// console.log(a===b);
